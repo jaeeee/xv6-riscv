f6104bdb5bd88cc62c288b604e0584b0c87d12d3
Hung Huynh <hhuyn019@sledge.cs.ucr.edu>
2021-01-15 16:00:51 -0800

    makefile

--- user/sleep.c
+++ user/sleep.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
     // atoi - changes string to integer
     int x = atoi(argv[1]);
     
-    if (argc != 2) {
+    if (argc != 2) { // checks if command is valid
         write(2, "Error message", strlen("Error message"));
     }
     
