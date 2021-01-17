db5dd511fb6794b92316d6d068c3a96c55896fd0
Jae <daniel.kwong.h@gmail.com>
2021-01-16 23:00:39 -0800

    trying sol

--- user/xargs.c
+++ user/xargs.c
@@ -1,52 +1,47 @@
 #include "kernel/types.h"
-#include "user/user.h"
 #include "kernel/stat.h"
 #include "kernel/param.h"
+#include "user/user.h"
 
-int main(int argc, char *argv[]) {
+int
+main(int argc, char *argv[])
+{
+  int argv_len = 0;
+  char buf[128] = {0};
+  char *max_argv[MAXARG];
+  for(int i = 1; i < argc; i++){
+      max_argv[i-1] = argv[i];
+  }
+  while(gets(buf, sizeof(buf))){
+    int buf_len = strlen(buf);
+    if(buf_len < 1) break;
+    buf[buf_len - 1] = 0;
 
-     char *max_argv_size[MAXARG];
-     int argv_length = 0;
-     char buffer[128] = {0};
+    argv_len = argc - 1;
+    char *x = buf;
+    while(*x){
+      while(*x && (*x == ' ')) *x++ = 0;
+      if(*x) max_argv[argv_len++] = x;
+      while(*x && (*x != ' ')) x++;
+    }
 
-      while(gets(buffer, sizeof(buffer))){
-          int buffer_length = strlen(buffer);
-          char *c = buffer;
-          if (buffer_length <= 0) {
-              break;
-          }
-          buffer[buffer_length - 1] = 0;
-          argv_length = argc - 1;
-          while (*c) {
-              while(*c && (*c == ' ')) {
-                  *c++ = 0;
-              }
-              if (*c) {
-                  max_argv_size[argv_length++] = c;
-              }
-              while (*c && (*c != ' ')) {
-                  c++;
-              } 
-          }
+    if (argv_len >= MAXARG){
+      printf("xargs too many args\n");
+      exit();
+    }
+    if (argv_len < 1){
+      printf("xargs too few args\n");
+      exit();
+    }
 
+    max_argv[argv_len] = 0;
+    if (fork() > 0) {
+        wait();
+    }else{
+        exec(max_argv[0], max_argv);
+        exit();
+    }
+  }
+  exit();
 
-          if (argv_length <= 0) {
-              printf("too few args (xargs)\n");
-              exit();
-          }
-          if (argv_length >= MAXARG) {
-              printf("too many args (xargs) \n");
-              exit();
-          }
-
-          max_argv_size[argv_length] = 0;
-          if (fork()) {
-              wait();
-          } else {
-              exec(max_argv_size[0], max_argv_size);
-              exit();
-          }
-      }
-
-    exit();
 }
