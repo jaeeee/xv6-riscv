0663eae072e3bea84367957f574ba061abb92ea0
Jae <daniel.kwong.h@gmail.com>
2021-01-16 22:56:35 -0800

    trying this

--- user/xargs.c
+++ user/xargs.c
@@ -10,24 +10,32 @@ int main(int argc, char *argv[]) {
 
       while(gets(buffer, sizeof(buffer))){
           int buffer_length = strlen(buffer);
-          char *c = buffer;
+        //   char *c = buffer;
+
+        char *x = buffer;
           if (buffer_length <= 0) {
               break;
           }
           buffer[buffer_length - 1] = 0;
           argv_length = argc - 1;
-          while (*c) {
-              while(*c && (*c == ' ')) {
-                  *c++ = 0;
-              }
-              if (*c) {
-                  max_argv_size[argv_length++] = c;
-              }
-              while (*c && (*c != ' ')) {
-                  c++;
-              } 
-            //   while 
-          }
+        //   while (*c) {
+        //       while(*c && (*c == ' ')) {
+        //           *c++ = 0;
+        //       }
+        //       if (*c) {
+        //           max_argv_size[argv_length++] = c;
+        //       }
+        //       while (*c && (*c != ' ')) {
+        //           c++;
+        //       } 
+        //     //   while 
+        //   }
+
+          while(*x){
+      while(*x && (*x == ' ')) *x++ = 0;
+      if(*x) max_argv_size[argv_length++] = x;
+      while(*x && (*x != ' ')) x++;
+    }
 
           if (argv_length <= 0) {
               printf("too few args (xargs)\n");
