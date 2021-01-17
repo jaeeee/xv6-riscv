1d53271e693159657d713ccaeb1effbbb71d3997
Jae <daniel.kwong.h@gmail.com>
2021-01-16 23:00:02 -0800

    add include

--- user/xargs.c
+++ user/xargs.c
@@ -1,5 +1,6 @@
 #include "kernel/types.h"
 #include "user/user.h"
+#include "kernel/stat.h"
 #include "kernel/param.h"
 
 int main(int argc, char *argv[]) {
@@ -10,32 +11,24 @@ int main(int argc, char *argv[]) {
 
       while(gets(buffer, sizeof(buffer))){
           int buffer_length = strlen(buffer);
-        //   char *c = buffer;
-
-        char *x = buffer;
+          char *c = buffer;
           if (buffer_length <= 0) {
               break;
           }
           buffer[buffer_length - 1] = 0;
           argv_length = argc - 1;
-        //   while (*c) {
-        //       while(*c && (*c == ' ')) {
-        //           *c++ = 0;
-        //       }
-        //       if (*c) {
-        //           max_argv_size[argv_length++] = c;
-        //       }
-        //       while (*c && (*c != ' ')) {
-        //           c++;
-        //       } 
-        //     //   while 
-        //   }
+          while (*c) {
+              while(*c && (*c == ' ')) {
+                  *c++ = 0;
+              }
+              if (*c) {
+                  max_argv_size[argv_length++] = c;
+              }
+              while (*c && (*c != ' ')) {
+                  c++;
+              } 
+          }
 
-          while(*x){
-      while(*x && (*x == ' ')) *x++ = 0;
-      if(*x) max_argv_size[argv_length++] = x;
-      while(*x && (*x != ' ')) x++;
-    }
 
           if (argv_length <= 0) {
               printf("too few args (xargs)\n");
